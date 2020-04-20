2a546c549936818b5d8d045f8e600d04db88515c
DinoBektesevic <dinob@uw.edu>
2020-04-20 14:46:41 -0700

    Problem 4 solution and an organizational mistake fixes.

--- HW1/HW1_code_solutions/mapFeature.py
+++ HW1/HW1_code_solutions/mapFeature.py
@@ -2,10 +2,10 @@
 def mapFeature(x1, x2):
     '''
     Maps the two input features to quadratic features.
-        
+
     Returns a new feature array with d features, comprising of
         X1, X2, X1 ** 2, X2 ** 2, X1*X2, X1*X2 ** 2, ... up to the 6th power polynomial
-        
+
     Arguments:
         X1 is an n-by-1 column matrix
         X2 is an n-by-1 column matrix
--- HW1/HW1_code_solutions/polyreg.py
+++ HW1/HW1_code_solutions/polyreg.py
@@ -11,17 +11,34 @@ import numpy as np
 #-----------------------------------------------------------------
 
 class PolynomialRegression:
-
     def __init__(self, degree=1, reg_lambda=1E-8):
         """
         Constructor
         """
-        #TODO
+        self.regLambda = reg_lambda
+        self.degree = degree
+        self.theta = None
+        self._mean = None
+        self._std = None
+
+
+    def _expandToDegree(self, X, degree=None):
+        """Expands the given column vector to a (n,d) matrix where elements are
+        powers of values x_i including the zero-th order.
+
+        [x1, ... x_n]^T = [[1, x1, x_1^2, ... x_1d^d],
+                                       ...
+                           [1, x_n, x_n^2, ... x_nd^d]]
+        """
+        if degree is None:
+            degree = self.degree
+        return (X[:,None]**np.arange(degree+1))[:, 0, :]
+
 
     def polyfeatures(self, X, degree):
         """
         Expands the given X into an n * d array of polynomial features of
-            degree d.
+        degree d.
 
         Returns:
             A n-by-d numpy array, with each row comprising of
@@ -32,7 +49,23 @@ class PolynomialRegression:
             X is an n-by-1 column numpy array
             degree is a positive integer
         """
-        #TODO
+        self.degree = degree
+        return self._expandToDegree(X, degree)[:, 1:]
+
+
+    def standardize(self, X, mean=None, std=None):
+        """Returns a standardized copy of the array using the given weights.
+
+        Standardization is performed by offsetting by mean and dividing by
+        variance on a per column basis.
+        """
+        mean = self._mean if mean is None else mean
+        std = self._std if std is None else std
+        standardized = []
+        for row in X:
+            standardized.append((row-mean)/std)
+        return np.vstack(standardized)
+
 
     def fit(self, X, y):
         """
@@ -46,7 +79,25 @@ class PolynomialRegression:
                 You need to apply polynomial expansion and scaling
                 at first
         """
-        #TODO
+        # expand to polynomial of degree d
+        X_ = self.polyfeatures(X, self.degree)
+
+        # standardize the matrix and remember the weights
+        self._mean = np.mean(X_, axis=0)
+        self._std = np.std(X_, axis=0)
+        X_ = self.standardize(X_)
+
+        # add a column of oens
+        X_ = np.c_[np.ones([len(X), 1]), X_]
+
+        # construct reg matrix
+        reg_matrix = self.regLambda * np.eye(self.degree+1)
+        reg_matrix[0, 0] = 0
+
+        # analytical solution (X'X + regMatrix)^-1 X' y
+        self.theta = np.linalg.pinv(X_.T.dot(X_) + reg_matrix).dot(X_.T).dot(y)
+        return self.theta
+
 
     def predict(self, X):
         """
@@ -56,7 +107,10 @@ class PolynomialRegression:
         Returns:
             an n-by-1 numpy array of the predictions
         """
-        # TODO
+        X_ = self.polyfeatures(X, self.degree)
+        X_ = self.standardize(X_)
+        X_ = np.c_[np.ones([len(X), 1]), X_]        
+        return X_.dot(self.theta)
 
 
 #-----------------------------------------------------------------
