7b3a99a394edb4190f5647bc7ba45556379c81d0
DinoBektesevic <dinob@uw.edu>
2020-04-23 16:33:20 -0700

    Adding B2 code, updating PDF.

--- HW1/HW1_code_solutions/ridge_regression.py
+++ HW1/HW1_code_solutions/ridge_regression.py
@@ -1,6 +1,8 @@
 import numpy as np
-from mnist import MNIST
 from scipy import linalg
+import matplotlib . pyplot as plt
+
+from mnist import MNIST
 
 
 def load_mnist_dataset(path="data/mnist_data/"):
@@ -22,7 +24,7 @@ def load_mnist_dataset(path="data/mnist_data/"):
     test : `np.array`
         test data normalized to 1
     testLabels : `np.array`
-        test data labels    
+        test data labels
     """
     mndata = MNIST("data/mnist_data/")
 
@@ -49,7 +51,7 @@ def one_hot(length, index):
     Returns
     -------
     arr : `np.array`
-        Array of zeros except for arr[index]=1. 
+        Array of zeros except for arr[index]=1.
     """
     arr = np.zeros(length)
     arr[index] = 1
@@ -57,7 +59,7 @@ def one_hot(length, index):
 
 
 def train(X, Y, lamb):
-    """Given data, labels and regularization constant lambda solves 
+    """Given data, labels and regularization constant lambda solves
 
     $$ W = (X^T X) + \lambda I $$
 
@@ -79,7 +81,6 @@ def train(X, Y, lamb):
         Matrix of weights that minimize the linear least squares.
     """
     n, d = X.shape
-
     a = np.dot(X.T, X) + lamb*np.eye(d)
     b = np.dot(X.T, Y)
     wHat = linalg.solve(a, b)
@@ -134,21 +135,21 @@ def calc_success_fraction(W, data, labels):
     fracCorrect : `float`
         Fraction of correctly predicted labels
     fracWrong : `float`
-        Fraction of incorrectly predicted labels 
+        Fraction of incorrectly predicted labels
     """
     n, d = data.shape
     labelDim = labels.shape[-1]
 
-    wrong = np.sum(np.abs(predict(W, data, labelDim) - labels))
+    wrong = np.sum(np.abs(predict(W, data, labelDim) - labels)) / 2.0
     # 2 is required because abs value will contribute double to the sum
-    fracWrong = wrong/(2.0*n)
+    fracWrong = wrong/n
     fracCorrect = 1 - fracWrong
 
     return fracCorrect, fracWrong
 
 
-def main(lambd=1e-4):
-    """Given the dimension of label space and regularization parameter value
+def mainA6(lambd=1e-4):
+    """Given the dimension of label space and regularization parameter value,
     trains a model on the MNIST train dataset, predicts the labels on the MNIST
     test dataset and calculates the fraction of wrongly predicted labels.
 
@@ -164,22 +165,227 @@ def main(lambd=1e-4):
     testErr : `float`
         Test error, fraction of incorrectly labeled test data
     """
-    xTrain, trainLabels, xTest, testLabels = load_mnist_dataset()
-    n, d = xTrain.shape
+    trainData, trainLabels, testData, testLabels = load_mnist_dataset()
+    n, d = trainData.shape
     labelDim = trainLabels.max() + 1 
 
-    yTrain = np.array([one_hot(labelDim, y) for y in trainLabels])
-    yTest  = np.array([one_hot(labelDim, y) for y in testLabels])
+    trainOneHot = np.array([one_hot(labelDim, y) for y in trainLabels])
+    testOneHot  = np.array([one_hot(labelDim, y) for y in testLabels])
 
-    wHat = train(xTrain, yTrain, lambd)
+    wHat = train(trainData, trainOneHot, lambd)
 
-    trainErr = calc_success_fraction(wHat, xTrain, yTrain)[-1]
-    testErr  = calc_success_fraction(wHat, xTest,  yTest)[-1]
+    trainErr = calc_success_fraction(wHat, trainData, trainOneHot)[-1]
+    testErr  = calc_success_fraction(wHat, testData,  testOneHot)[-1]
     print(f"Train error: {trainErr}")
     print(f"Test error: {testErr}")
 
     return trainErr, testErr
 
 
+def transform(data, p, G=None, b=None):
+    """Returns the transformation h(x) := R^d --> R^p of the given data x.
+
+    Transformation is defined as :
+        h(x) = cos(Gx + b)
+    where
+        G = Normal(mu=0, std=sqrt(0.1)) \in R^{p,d}
+    and
+        b = Uniform(0, 2pi) \in R^p
+
+    Parameters
+    ----------
+    data : `np.array`
+        Data to transform, can be a 
+    p : `int`
+        Dimension of the image of the transformation.
+    G : `np.array`, optional
+        Unless G is provided, it will be created as defined above
+    b : `np.array`, optional
+        Unless b is provided, one will be created as defined above. Note that
+        it is expected that b is provided as a vector from R^p. It will be
+        expanded to R^{p, n}, where n is the dimension of the data, so that b
+        can be added to the given data. The unexpanded b is returned.
+
+    Returns
+    -------
+    H : `np.array`
+        Transformed data
+    G : `np.array`
+        Matrix of random elements drawn from a normal distribution.
+    b : `np.array`
+        Vector of random elements drawn from a uniform distribution.
+    """
+    n, d = data.shape
+
+    if G is None:
+        G = np.random.normal(0, 0.1**1/2, (p, d))
+    if b is None :
+        b = np.random.uniform(0, 2*np.pi, p)
+    B = np.vstack([b]*n)
+
+    H = np.cos(np.dot(data, G.T) + B)
+    return H, G, b
+
+
+def split(data, labels, ratio):
+    """Randomly splits the given data set and labels into two disjunct
+    data/labels sets according to the given ratio.
+
+    Parameters
+    ----------
+    data : `np.array`
+        Data that will be split.
+    labels : `np.array`
+        Labels for the corresponding data.
+    ratio : `float`
+        Number from <0, 1] setting the sizes of the split portions of the given
+        data set.
+    """
+    n, d = data.shape
+
+    nTest = int(n*ratio)
+    nValidation = n - nTest
+
+    shuffled = np.random.permutation(np.arange(0, n))
+
+    testData = data[shuffled[:nTest]]
+    testLabels = labels[shuffled[:nTest]]
+    valData = data[shuffled[nTest:]]
+    valLabels = labels[shuffled[nTest:]]
+
+    return testData, testLabels, valData, valLabels
+
+
+def mainB2(lambd=1e-4, splitFraction=0.8):
+    """Given the dimension of label space and regularization parameter value,
+    loads the MNIST train and test datasets, splits the train dataset into the
+    train and validation datasets 80:20 in size, transforms the train and
+    validation datasets into cosine space (see help(transform)), trains a model,
+    and finally predicts the labels for both train and validation datasets for
+    various values of p, where p is the dimension of the transformation image.
+
+    Outputs the p, training accuracy, training error, validation accuracy and
+    validation error into a file "B2Accuracy" as a space separated columnar
+    data file. 
+
+    Parameters
+    ----------
+    lambd : `float`, optional
+        Regularization parameter (lamda), by default 1e-4
+    splitFraction : `float`, optional
+        Fraction of MNIS training set that will remain a trainign set. The
+        1-splitFraction gives the fraction of the MNIST training set that is
+        separated into a validation test set.
+
+    Returns
+    -------
+    outfile : `file`
+        Writes a "B2Accuracy" file containing errors and accuracies to disk,
+        in the directory the code was run from.
+    """
+    trainData, trainLabels, testData, testLabels = load_mnist_dataset()
+    labelDim = trainLabels.max() + 1 
+    trainOneHots = np.array([one_hot(labelDim, y) for y in trainLabels])
+    testOneHots  = np.array([one_hot(labelDim, y) for y in testLabels])
+
+    trainData, trainLabels, valData, valLabels = split(trainData, trainOneHots, splitFraction)
+    
+    ps = np.hstack(np.arange(1, 7000, 50))
+    with open("B2Accuracy", "w") as outfile:
+        outfile.write("# p trainAccuracy trainError testAccuracy testError \n")
+        for p in ps:
+            # throw away G's and B's we don't need for memory reasons
+            trainH, trainG, trainB = transform(trainData, p)
+            valH, _, _ = transform(valData, p, G=trainG, b=trainB)
+
+            wHat = train(trainH, trainLabels, lambd)
+
+            trainAcc, trainErr = calc_success_fraction(wHat, trainH, trainLabels)
+            valAcc, valErr  = calc_success_fraction(wHat, valH,  valLabels)
+            outfile.write(f"{p} {trainAcc} {trainErr} {valAcc} {valErr}\n")
+
+
+def plotB2():
+    """Plots B2Accuracy data, specifically the training and validation error
+    columns, and displays the plot. 
+    """
+    data = np.loadtxt("B2Accuracy")
+
+    ps = data[:, 0]
+    testAcc = data[:, 1]
+    testErr = data[:, 2]
+    valAcc = data[:, 3]
+    valErr = data[:, 4]
+
+    plt.plot(ps, testErr, label="Test error")
+    plt.plot(ps, valErr, label="Validation error")
+
+    plt.xlabel("p")
+    plt.ylabel("Classification Error")
+    plt.legend()
+    plt.show()
+
+
+def confidence_intervalB2(lambd=1e-4, splitFraction=0.8):
+    """Given the dimension of label space and regularization parameter value,
+    loads the MNIST train and test datasets, splits the train dataset into the
+    train and validation datasets 80:20 in size, transforms the train and
+    validation datasets into cosine space (see help(transform)), trains a model,
+    and finally predicts the labels for both train and validation datasets for
+    various values of p, where p is the dimension of the transformation image.
+
+    Outputs the p, training accuracy, training error, validation accuracy and
+    validation error into a file "B2Accuracy" as a space separated columnar
+    data file. 
+
+    Parameters
+    ----------
+    lambd : `float`, optional
+        Regularization parameter (lamda), by default 1e-4
+    splitFraction : `float`, optional
+        Fraction of MNIS training set that will remain a trainign set. The
+        1-splitFraction gives the fraction of the MNIST training set that is
+        separated into a validation test set.
+
+    Returns
+    -------
+    outfile : `file`
+        Writes a "B2Accuracy" file containing errors and accuracies to disk,
+        in the directory the code was run from.
+    """
+    trainData, trainLabels, testData, testLabels = load_mnist_dataset()
+    labelDim = trainLabels.max() + 1 
+    trainOneHots = np.array([one_hot(labelDim, y) for y in trainLabels])
+    testOneHots  = np.array([one_hot(labelDim, y) for y in testLabels])
+
+    data = np.loadtxt("B2Accuracy")
+    ps = data[:, 0]
+    valAcc = data[:, 3]
+    valErr = data[:, 4]
+
+    minValErr = valErr.min()
+    minIndex = np.where(valErr == valErr.min())[0][0]
+    print(f"Validation error minimum is {minValErr} at p={int(ps[minIndex])}.")
+    print(f"Validation accuracy at hat p is {valAcc[minIndex]}.")
+    p = int(ps[minIndex])
+
+    trainData, trainLabels, valData, valLabels = split(trainData, trainOneHots, splitFraction)
+    trainH, G, b = transform(trainData, p)
+    testH, _, _ = transform(testData, p, G=G, b=b)
+
+    wHat = train(trainH, trainLabels, lambd)
+
+    testAcc, testErr = calc_success_fraction(wHat, testH, testOneHots)
+
+    d = 0.05
+    m = len(testH)
+    hoeff = np.sqrt(np.log(2.0/d)/(2.0*m))
+    print(f"Testing error at hat p is {testErr}.")
+    print(f"For delta={d}, m={m}, we  have the 95% confidence interval: [{testErr-hoeff, testErr+hoeff}].")
+
+
 if __name__ == "__main__":
-    main()
+    #mainA6()
+    #mainB2()
+    #plotB2()
+    confidence_intervalB2()
