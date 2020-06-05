11f706eb22fb7835e8124a79f3cdfcdf04d7f415
DinoBektesevic <dinob@uw.edu>
2020-06-04 21:13:52 -0700

    A3 solved.

--- HW4/HW4_code_solutions/autoencoders.py
+++ HW4/HW4_code_solutions/autoencoders.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib import cm
 
 import torch
 from torch import nn, optim
@@ -11,19 +12,12 @@ from torchvision import datasets, transforms
 from torchvision.utils import make_grid
 
 
-batch_size   = 128  # input batch size for training
-epochs       = 10   # number of epochs to train
-seed         = 1    # random seed
-lr           = 1e-3 # learning rate
-img_size     = 28*28 # MNIST images are 28x28
+torch.manual_seed(0)
+np.random.seed(0)
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-torch.manual_seed(seed)
-np.random.seed(seed)
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-
-def load_mnist_dataset(path="data/mnist_data/", digit=None):
+def load_mnist_dataset(path="data/mnist_data/", digit=None, batchSize=1):
     """Loads MNIST data located at path.
 
     MNIST data are 28x28 pixel large images of numbers.
@@ -31,18 +25,24 @@ def load_mnist_dataset(path="data/mnist_data/", digit=None):
     Parameters
     ----------
     path : `str`
-        path to the data directory
+        Path to the data directory
+    digit : `int` or `None`, optional
+        Load only data for a specific digit.
+    batchSize: `int`, optional
+        Batch size.
 
     Returns
     -------
-    train : `torch.tensor`
-        train data normalized to 1
-    trainlabels : `torch.tensor`
-        train data labels
-    test : `torch.tensor`
-        test data normalized to 1
-    testLabels : `torch.tensor`
-        test data labels
+    train : `torch.DataLoader`
+        A generator that will shuffle the data at every iteration, yields train
+        datasets
+    trainlabels : `torch.DataLoader`
+        A generator that returns the test dataset.
+
+    Notes
+    -----
+    Data are normalized upon loading to the mean of the dataset.
+    Data are downloaded if not present at path.
     """
     trans = transforms.Compose([
         transforms.ToTensor(),
@@ -53,162 +53,432 @@ def load_mnist_dataset(path="data/mnist_data/", digit=None):
 
     trainMask, testMask = np.arange(len(train)), np.arange(len(test))
     if digit is not None:
-
         trainMask = np.where(train.targets == digit)[0]
         testMask = np.where(test.targets == digit)[0]
 
     maskTrain = datutils.Subset(train, trainMask)
     maskTest = datutils.Subset(test, testMask)
-    trainLoader = datutils.DataLoader(maskTrain, batch_size=batch_size, shuffle=True)
-    testLoader = datutils.DataLoader(maskTest, batch_size=batch_size, shuffle=False)
+    trainLoader = datutils.DataLoader(maskTrain, batch_size=batchSize, shuffle=True)
+    testLoader = datutils.DataLoader(maskTest, batch_size=batchSize, shuffle=False)
 
     return trainLoader, testLoader
 
 
-train_loader, test_loader = load_mnist_dataset()
+class MNISTAutoencoder(nn.Module):
+    """MNIST Autoencoder Module.
 
-
-
-class Autoencoder(nn.Module):
-    def __init__(self, med_size=32, lat_size=2):
+    Parameters
+    ----------
+    encode: `torch.nn.Sequential`
+        Encoding steps
+    decode: `torch.nn.Sequential`
+        Decoding steps
+    imgSize: `int`, optional
+        The image dimensions multiplied. Defaults to 28*28
+    """
+    def __init__(self, encode, decode, *args, **kwargs):
         super().__init__()
-
-        self.enMu = nn.Linear(img_size, med_size)
-        self.enVar = nn.Linear(med_size, lat_size)
-
-        self.deMu = nn.Linear(lat_size, med_size)
-        self.deVar = nn.Linear(med_size, img_size)
-
-        self.lat_size = lat_size
-
-    def encode(self, x):
-        lat1 = functional.relu(self.enMu(x))
-        return self.enVar(lat1)
-
-    def decode(self, z):
-        lat2 = functional.relu(self.deMu(z))
-        return self.deVar(lat2)
+        self.encode = encode
+        self.decode = decode
+        self.imgSize = kwargs.pop("imgSize", 28*28)
 
     def forward(self, x):
-        z = self.encode(x.view(-1, img_size))
-        return self.decode(z), z
-    
+        """Encode the given data and then decode it.
+
+        Parameters
+        -----------
+        x : `torch.tensor`
+            Data to encode
+
+        Returns
+        -------
+        decoded : `torch.tensor`
+            The decoded encoded data.
+        """
+        z = self.encode(x.view(-1, self.imgSize))
+        return self.decode(z)
+
     def loss(self, x):
-        reconstructed, z = self.forward(x)
-        loss = functional.mse_loss(reconstructed, x.view(-1, img_size))
+        """Given the data, alculate MSE loss of the reconstruction. Ensure the
+        autoencoder has been trained.
+
+        Parameters
+        -----------
+        x : `torch.tensor`
+            Data 
+
+        Returns
+        -------
+        loss : `nn.MSELoss`
+            MSE Loss
+        """
+        reconstructed = self.forward(x)
+        loss = functional.mse_loss(reconstructed, x.view(-1, self.imgSize))
         return loss
 
 
+class A3aAutoencoder(MNISTAutoencoder):
+    """Simple single linear encode and decode layer autoencoder.
 
-class Autoencoder2(nn.Module):
-    def __init__(self, med_size=128):
-        super().__init__()
-        self.en1 = nn.Linear(img_size, med_size)
-        self.de1 = nn.Linear(med_size, img_size)
-
-        self.lat_size = 1
-
-    def encode(self, x):
-        return functional.relu(self.en1(x))
-
-    def decode(self, z):
-        return functional.relu(self.de1(z))
-
-    def forward(self, x):
-        z = self.encode(x.view(-1, img_size))
-        return self.decode(z), z
-    
-    def loss(self, x):
-        reconstructed, latent = self.forward(x)
-        loss = functional.mse_loss(reconstructed, x.view(-1, img_size))
-        return loss
+    Parameters
+    ----------
+    imgSize : `int`, optional
+        Size of the images. Default 28*28
+    hiddenSize : `int`, optional
+        Size of the encoding layer. Default 64
+    """
+    def __init__(self, imgSize=28*28, hiddenSize=64, **kwargs):
+        super().__init__(
+            encode = nn.Sequential(nn.Linear(imgSize, hiddenSize)),
+            decode = nn.Sequential(nn.Linear(hiddenSize, imgSize)),
+            imgSize = imgSize,
+            **kwargs
+        )
+        self.hiddenSize = hiddenSize
 
 
-def train(model, optimizer, epoch):
+class A3bAutoencoder(MNISTAutoencoder):
+    """Simple single non-linear (ReLU) encode and decode layer autoencoder.
+
+    Parameters
+    ----------
+    imgSize : `int`, optional
+        Size of the images. Default 28*28
+    hiddenSize : `int`, optional
+        Size of the encoding layer. Default 64
+    """
+
+    def __init__(self, imgSize=28*28, hiddenSize=64, **kwargs):
+        super().__init__(
+            encode = nn.Sequential( nn.Linear(imgSize, hiddenSize), nn.ReLU() ),
+            decode = nn.Sequential( nn.Linear(hiddenSize, imgSize), nn.ReLU() ),
+            imgSize = imgSize,
+            **kwargs
+        )
+        self.hiddenSize = hiddenSize
+        
+
+
+def train(dataLoader, model, optimizer, epoch, verbosity=20):
+    """Trains the model using the given batched data. Calculates loss for each
+    batch as well as the total average loss across the epoch and prints them.
+
+    Parameters
+    ----------
+    dataLoader: `torch.DataLoader`
+        Generator that returnes batched train data.
+    model : `obj`
+        One of the Autoencoders or some other nn.Module with a loss method.
+    optimizer : `obj`
+        One of pytorches optimizers (f.e. torch.optim.Adam)
+    epoch : `int`
+        What epoch is this training step performing, used to calculate loss
+    verbosity: `int`
+        How often are batch losses printed, larger number means less prints.
+        Epoch average loss is always printed.
+    """
     trainLoss = 0
-    for i, (data, labels) in enumerate(train_loader):
-        data = data.to(device)
+    for i, (data, labels) in enumerate(dataLoader):
+        data = data.to(DEVICE)
+
         optimizer.zero_grad()
         loss = model.loss(data)
         loss.backward()
         trainLoss += loss.item()
         optimizer.step()
 
-        if i % 20 == 0:
+        if i % verbosity == 0:
             # data loader length is number of batches that fit in the dataset.
             # The length of data, loaded by loader, is at most the batch size
             # and the length of the dataset is the actual number of data points
             # used (f.e. the total number mnist images of the same digit)
-            nBatches = len(train_loader)
+            nAll, nBatches = len(dataLoader.dataset), len(dataLoader)
             nBatch = len(data)
-            nAll = len(train_loader.dataset)
-            print(f'    Train Epoch: {epoch} [{i*nBatch}/{nAll} '
-                  f'({100* i/nBatches:.04}%)]        Loss: {loss.item()/nBatch:.6f}')
+            print(f'    Train Epoch: {epoch:<4} [{i*nBatch:>6}/{nAll:>6} '
+                  f'({100.0*i/nBatches:<5.4}%)]    Loss: {loss.item()/nBatch:>10.8f}')
 
-    print('Epoch: {} Average loss: {:.4f}'.format(
-          epoch, trainLoss / len(train_loader.dataset)))
+    print('Epoch: {} Average loss: {:.8f}'.format(
+          epoch, trainLoss / len(dataLoader.dataset)))
 
 
-def test(model, optimizer, epoch):
+def test(dataLoader, model, optimizer):
+    """Calculate and print test losses.
+
+    Parameters
+    ----------
+    dataLoader: `torch.DataLoader`
+        Generator that returnes batched train data.
+    model : `obj`
+        One of the Autoencoders or some other nn.Module with a loss method.
+    optimizer : `obj`
+        One of pytorches optimizers (f.e. torch.optim.Adam)
+    """
     testLoss = 0
     with torch.no_grad():
-        for i, (data, labels) in enumerate(test_loader):
-            data = data.to(device)
-            reconBatch = model(data)[0]
+        for i, (data, labels) in enumerate(dataLoader):
+            data = data.to(DEVICE)
             testLoss += model.loss(data).item()
+    testLoss /= len(dataLoader.dataset)
+    print(f"Test set loss: {testLoss:.4f}")
 
-    testLoss /= len(test_loader.dataset)
-    print('Test set loss: {:.4f}'.format(testLoss))
 
+def learn(trainDataLoader, testDataLoader, model, epochs, learningRate, verbosity=20):
+    """Trains a model for n epochs using Adam optimizer, and then estimates
+    test dataset losses.
 
-def learn(model, epochs):
-    optimizer = optim.Adam(model.parameters(), lr=lr)
+    Parameters
+    ---------
+    trainDataLoader: `torch.DataLoader`
+        Generator that returnes batched train data.
+    testDataLoader: `torch.DataLoader`
+        Generator that returnes batched test data.
+    model : `obj`
+        One of the Autoencoders or some other nn.Module with a loss method.
+    epochs : `int`
+        Number of epochs to train for.
+    learningRate : `float`
+        Learning rate of the Adam optimizer.
+    verbosity: `int`
+        How often are batch losses printed, larger number means less prints.
+        Epoch average loss is always printed.
+    """
+    optimizer = optim.Adam(model.parameters(), lr=learningRate)
     for epoch in range(1, epochs + 1):
-        train(model, optimizer, epoch)
-        test(model, optimizer, epoch)
+        train(trainDataLoader, model, optimizer, epoch, verbosity=verbosity)
+        test(testDataLoader, model, optimizer)
 
 
-def show(img):
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1,2,0)), interpolation='nearest')
-    plt.axis("off")
+def showimages(images, interpolation="nearest"):
+    """Given a list of images (np.array or torch.tensor) creates a figure with
+    sufficient number of columns and plots each image in a separate one.
+
+    Parameters
+    ----------
+    images : `list` or `tuple`
+        List of images to plot
+
+    Returns
+    -------
+    fig : `matplotlib.pyplot.Figure`
+        Figure
+    axes : `matplotlib.pyplot.Axes`
+        Axes
+    """
+    fig, axes = plt.subplots(1, len(images), sharex=True, sharey=True)
+    axes = np.array([axes]) if len(images) == 1 else axes
+    for img, ax in zip(images, axes.ravel()):
+        ax.imshow(img, interpolation=interpolation)
+        ax.set_axis_off()
+
+    return fig, ax
+
+
+def plot_reconstructions(dataLoader, model):
+    """Given a trained model reconstructs a single example of each of the unique
+    labels present in the data and plots them side by side.
+
+    Parameters
+    ----------
+    dataLoader : `torch.DataLoader`
+        Generator that returns batched data
+    model : `obj`
+        One of the Autoencoders, or some other nn.Module.
+    """
+    # get unique digits in the dataset via this monstrosity. First dataset is
+    # the subset, second dataset is the whole dataset, the targets are the
+    # labels of that dataset. We can only plot for the digits we trained for,
+    # so sample unique digits from the subset. Convert to numpy to get non-
+    # tensor values and finally list gets us .pop()
+    allData = dataLoader.dataset.dataset
+    subsetIdxs = dataLoader.dataset.indices
+    digits = list(allData.targets[subsetIdxs].unique().numpy())
+    stacks = []
+    for data, label in allData:
+        if label == digits[0]:
+            with torch.no_grad():
+                reconstruction = model(data)
+            stack = torch.cat([data[0], reconstruction.view(28, 28)])
+            stacks.append(stack)
+            digits.pop(0)
+        if len(digits) == 0:
+            break
+    return showimages(stacks)
+
+
+def A3a(epochs=5, learningRate=1e-3, batchSize=128):
+    """Loads MNIST train and test datasets, batches them, trains a single layer
+    linear Autoencoder, reports on the train and test losses and plots the
+    reconstructons.
+
+    Parameters
+    ----------
+    epochs : `int`, optional
+       Number of training epochs. Default: 
+    learningRate : `float`, optional
+       Optimizers learning rate Default: 1e-3.
+    batchSize : `int`, optional
+        Size of the batched dataset. Default: 128
+    """
+    trainLoader, testLoader = load_mnist_dataset(batchSize=batchSize)
+
+    plots, latentSizes = [], (32, 64, 128)
+    for latentSize in latentSizes:
+        AC = A3aAutoencoder(latentSize=latentSize).to(DEVICE)
+        learn(trainLoader, testLoader, AC, epochs, learningRate)
+        fig, axes = plot_reconstructions(trainLoader, AC)
+        fig.suptitle("Digit reconstruction, linear \n"
+                     f"(h={latentSize}, Nbatch={batchSize}, epochs={epochs}, "
+                     f"lr={learningRate})")
+        plots.append((fig, axes))
     plt.show()
 
 
-def show_recon(model, n=9):
-    with torch.no_grad():
-        for i, (data, labels) in enumerate(train_loader):
-            if i == 0:
-                batchReconstructions = model(data)[0]
-                comparison = torch.cat([data[:n],
-                                      batchReconstructions.view(batch_size, 1, 28, 28)[:n]])
-                show(make_grid(comparison, nrow=n))
+def A3b(epochs=5, learningRate=1e-3, batchSize=128):
+    """Loads MNIST train and test datasets, batches them, trains a single layer
+    non-linear Autoencoder, reports on the train and test losses and plots the
+    reconstructons.
+
+    Parameters
+    ----------
+    epochs : `int`, optional
+       Number of training epochs. Default: 
+    learningRate : `float`, optional
+       Optimizers learning rate Default: 1e-3.
+    batchSize : `int`, optional
+        Size of the batched dataset. Default: 128
+    """
+    trainLoader, testLoader = load_mnist_dataset(batchSize=batchSize)
+
+    plots, latentSizes = [], (32, 64, 128)
+    for latentSize in latentSizes:
+        AC = A3bAutoencoder(latentSize=latentSize).to(DEVICE)
+        learn(trainLoader, testLoader, AC, epochs, learningRate)
+        fig, axes = plot_reconstructions(trainLoader, AC)
+        fig.suptitle("Digit reconstruction, non-linear \n"
+                     f"(h={latentSize}, Nbatch={batchSize}, epochs={epochs}, "
+                     f"lr={learningRate})")
+        plots.append((fig, axes))
+    plt.show()
 
 
-def show_encoding(model, axes=(0,1)):
-    from matplotlib import cm
+def A3():
+    """Runs part a and b of problem A3 in HW4"""
+    A3a()
+    A3b()
+
+
+if __name__ == "__main__":
+    A3()
+
+
+
+
+
+
+
+
+
+class MyAutoencoder(MNISTAutoencoder):
+    def __init__(self, imgSize=28*28, hiddenSize=128, latSize=2, **kwargs):
+        super().__init__(
+            encode = nn.Sequential(
+                nn.Linear(imgSize, hiddenSize),
+                nn.ReLU(),
+                nn.Linear(hiddenSize, latSize)
+            ),
+            decode = nn.Sequential(
+                nn.Linear(latSize, hiddenSize),
+                nn.ReLU(),
+                nn.Linear(hiddenSize, imgSize)
+            ),
+            imgSize = imgSize,
+            hiddenSize = hiddenSize,
+            latentSize = latSize,
+            **kwargs
+        )
+
+    def forward_mu(self, x):
+        return self.encode(x.view(-1, self.imgSize))
+
+
+class MyAutoencoder(MNISTAutoencoder):
+    def __init__(self, imgSize=28*28, h1=128, h2=64, latSize=2, **kwargs):
+        super().__init__(
+            encode = nn.Sequential(
+                nn.Linear(imgSize, h1),
+                nn.ReLU(),
+                nn.Linear(h1, h2),
+                nn.ReLU(),
+                nn.Linear(h2, latSize)
+            ),
+            decode = nn.Sequential(
+                nn.Linear(latSize, h2),
+                nn.ReLU(),
+                nn.Linear(h2, h1),
+                nn.Linear(h1, imgSize)
+            ),
+            imgSize = imgSize,
+            **kwargs
+        )
+
+    def forward_mu(self, x):
+        return self.encode(x.view(-1, self.imgSize))
+
+
+
+
+def plot_latent(dataLoader, model):
+
     digits = range(10)
     colors = [cm.tab10(digit/10.0) for digit in digits]
+    coldict = {d:c for d, c in zip(digits, colors)}
+
+    # fake legend
     for d, c in zip(digits, colors):
         plt.scatter(-100, -100, c=[c], label=d)
     plt.legend()
-    for i, (data, labels) in enumerate(train_loader):
+
+    # get the latent space values and plot them in colors of digits
+    for i, (data, labels) in enumerate(dataLoader):
         with torch.no_grad():
-            data = data.to(device)
-            mu = model(data.view(-1, 784))[1]
+            data = data.to(DEVICE)
+            mu = model.forward_mu(data.view(-1, 784))
             for digit, color in zip(digits, colors):
                 digitMask = labels == digit
-                plt.scatter(mu[:,axes[0]][digitMask], mu[:,axes[1]][digitMask],
-                            c=[color], alpha=0.1)
+                plt.scatter(mu[:,0][digitMask], mu[:,1][digitMask],
+                            c=[color], alpha=0.15)
 
-    plt.xlim(-2, 2)
-    plt.ylim(0, 5)
+    plt.title("Autoencoders Latent space")
+    plt.xlabel("Dimension 1 (mu, probably)")
+    plt.ylabel("Dimension 1 (variance, probably)")
+    plt.xlim(-15, 15)
+    plt.ylim(-5, 5)
     plt.show()
 
 
 
-AC = Autoencoder().to(device)
-learn(AC, epochs)
-show_recon(AC)
-show_encoding(AC)
-plt.show()
+def extra_autoencoder(epochs=10, learningRate=1e-3, batchSize=256):
+    trainLoader, testLoader = load_mnist_dataset(batchSize=batchSize)
+
+    hiddenDim, latDim = 64, 2
+    AC = MyAutoencoder(hiddenSize=hiddenDim, latSize=latDim).to(DEVICE)
+    learn(trainLoader, testLoader, AC, epochs, learningRate)
+    fig, axes = plot_reconstructions(trainLoader, AC)
+    fig.suptitle("Digit reconstruction, non-linear \n"
+                 f"(h={latDim}, Nbatch={batchSize}, epochs={epochs}, "
+                 f"lr={learningRate})")
+    plt.show()
+
+    plot_latent(trainLoader, AC)
+    plt.show()
+
+
+#extra_autoencoder()
+
+
+
+
+
+
+        
+
